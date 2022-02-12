27d567eba538ed3e0180300041ed8f004752071b
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2022-02-12 17:47:56 -0500

    update

---
+++ src/main/java/com/bank/passiveTransaction/controller/PassiveTransactionController.java
@@ -0,0 +1,30 @@
+package com.bank.passiveTransaction.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.passiveTransaction.service.PassiveTransactionService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/passive-transaction")
+public class PassiveTransactionController {
+	
+	private final PassiveTransactionService passiveTransactionService;
+
+	@GetMapping
+	public String test() {
+		return "hello world";
+	}
+	
+}
